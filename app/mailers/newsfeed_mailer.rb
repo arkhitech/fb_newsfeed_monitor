3fa60346f523df87ccbe7f963318d77225cd6647
Abuzar <abuzar.hasan@arkhitech.com>
2013-11-07 11:53:04 +0500

    testing scheduler on heroku

--- app/mailers/newsfeed_mailer.rb
+++ app/mailers/newsfeed_mailer.rb
@@ -1,11 +1,10 @@
 class NewsfeedMailer < ActionMailer::Base
   
-  default from: "abuzar.hasan@arkhitech.com"
+  default from: "info@arkhitech.com"
   
   def send_newsfeed(user, feed)
     @user = user
     @feed = feed
-    #    @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Newsfeed Monitor')
   end
   
