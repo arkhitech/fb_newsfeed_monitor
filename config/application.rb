2ec10374b5743f916e1adce1c4dda067743c9ee4
Babar Rehman <babar.rehman@arkhitech.com>
2013-11-11 16:47:39 +0500

    redirection is handled now.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
 module Newsfeed
   class Application < Rails::Application
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.ico)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Newsfeed::Application.routes.draw do
   #devise_for :users
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" },:skip=>[:sessions]
-  
-  
-devise_scope :user do
-  get 'sign_in', :to => 'users#welcome', :as => :new_user_session
-  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-end
-  
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }  
   root :to => 'users#welcome'
   resources :users
   resources :search_phrases
