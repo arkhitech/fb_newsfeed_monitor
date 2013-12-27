9d87cd03b475171991aaa279696f8c0f04739900
Asim Mushtaq <asim.mushtaq@arkhitech.com>
2013-12-27 13:30:57 +0500

    adds test for edit, error and new templates of search phrases, corrects bug in _edit.html.erb where search_phrases in form was changed to @search_phrases

--- spec/views/search_phrases/_error.html.erb_spec.rb
+++ spec/views/search_phrases/_error.html.erb_spec.rb
@@ -1,5 +1,37 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+require 'spec_helper'
 
-puts "Hello World"
+describe "search_phrases/_error.html.erb" do
+  include Devise::TestHelpers
+  User.destroy_all
+  
+    before :all do
+    @user = User.find_by_name('testuser')
+    @user ||= begin
+      user = User.new(email: "test@gmail.com",
+        encrypted_password: "$2a$10$TIJrH0tmQ7VuDeBVBIYhJ......................",
+        provider: "facebook", uid: "10000697585", name: "testuser",
+        fb_token: "AUExuiUkFoBAEsYdxTfU1ONYanI55Sx5ThBTjo7qnKLiZ")
+      user.save!
+      user
+    end
+  end
+  
+  before  :each do
+    User.stub(:find_for_facebook_oauth).and_return(@user)
+    sign_in @user
+  end
+  
+  before :all do
+    @search_phrase = SearchPhrase.find_by_user_id(@user.id)
+    @search_phrase ||= begin
+      search_phrase= SearchPhrase.new(keyword: "testkeyword",user_id: @user.id) #never hard code
+      search_phrase.save!
+      search_phrase
+    end
+  end
+
+   it "creates render error partial page" do
+       view.stub(:render).with(hash_including(:partial => "search_phrases/error"))
+    end
+  
+end
--- spec/views/search_phrases/index.html.erb_spec.rb
+++ spec/views/search_phrases/index.html.erb_spec.rb
@@ -1,5 +1,55 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+require 'spec_helper'
 
-puts "Hello World"
+describe "search_phrases/_index.html.erb" do
+  include Devise::TestHelpers
+  User.destroy_all
+  
+  before :all do
+    @user = User.find_by_name('testuser')
+    @user ||= begin
+      user = User.new(email: "test@gmail.com",
+        encrypted_password: "$2a$10$TIJrH0tmQ7VuDeBVBIYhJ......................",
+        provider: "facebook", uid: "10000697585", name: "testuser",
+        fb_token: "AUExuiUkFoBAEsYdxTfU1ONYanI55Sx5ThBTjo7qnKLiZ")
+      user.save!
+      user
+    end
+  end
+  
+  before  :each do
+    User.stub(:find_for_facebook_oauth).and_return(@user)
+    sign_in @user
+  end
+  
+    before :all do
+      @search_phrase = SearchPhrase.find_by_user_id(@user.id)
+      @search_phrase ||= begin
+        search_phrase= SearchPhrase.new(keyword: "testkeyword",user_id: @user.id) #never hard code
+        search_phrase.save!
+        search_phrase
+      end
+    end
+
+  before(:each) do
+    @search_phrase_new = assign(:search_phrase, stub_model(SearchPhrase)).as_new_record.as_null_object
+  end
+  
+  it "renders the template '_index.html.erb_spec' with errors" do
+    
+    puts "*--Old> #{@search_phrase.to_yaml}"
+    puts "*--New> #{@search_phrase_new.to_yaml}"
+    render
+    expect(view).to render_template("search_phrases/_error", "search_phrases/_index")
+  end
+    
+    
+  it "renders the partial form '_index.html.erb_spec'" do
+    render
+    puts "*--> #{rendered}"
+    rendered.should have_selector('form') do |form|
+      form.should have_selector('input',:type => "text",:name=>'search_phrase[keyword]',:id=>'search_phrase_keyword')
+      form.should have_selector('input',:type=>'submit',:value=>'Update')
+    end
+  end
+  
+end
